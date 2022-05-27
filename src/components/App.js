e952850a6649a8c398c8e4c32202b6a867a27650
Borek78 <dalibor.pavlik@gmail.com>
2022-05-27 13:13:54 +0200

    prvni

--- src/components/App.js
+++ src/components/App.js
@@ -82,6 +82,7 @@ function App() {
           .toLowerCase()
           .includes(a.toLowerCase());
       });
+      console.log(newContactList);
       setSearchResults(newContactList);
       console.log(searchResults);
     } else {
